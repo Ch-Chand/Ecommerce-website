5e01984395599ac92fcf1491950d74df6e5c5cec
Chand <meetchchand@gmail.com>
2020-12-03 11:32:14 +0500

    Changed Document title

--- php/signup.php
+++ php/signup.php
@@ -1,26 +1,31 @@
 <?php
-$name = $_POST["name"];
-$email = $_POST["email"];
-$pwd = $_POST["pwd"];
-$chk =$_POST["check"];
 
-echo $chk;
+$chk = $_POST["check"];
+if ($chk == 'on') {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $pwd = $_POST["pwd"];
 
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $database = "sablayja";
 
-// $con = mysqli_connect($servername, $username, $password, $database) or die("Not Connected");
-// if ($con->connect_error == false) {
-//     $query = "SELECT * FROM 'login' WHERE 'email'='$email'";
-//     $res = mysqli_query($con, $query);
-//     if (mysqli_num_rows($res) > 0) {
-//         echo "Email Already Exist....";
-//     } else {
-//         $query = "INSERT INTO `login` (`id`, `name`, `email`, `password`) VALUES ('1','$name','$email','$pwd')";
-//         mysqli_query($con, $query);
-//         echo "Created....";
-//     }
-//}
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "sablayja";
+
+    $con = mysqli_connect($servername, $username, $password, $database) or die("Not Connected");
+    if ($con->connect_error == false) {
+        $query = "SELECT * FROM 'login' WHERE 'email'='$email'";
+        $res = mysqli_query($con, $query);
+        if (mysqli_num_rows($res) > 0) {
+            echo "Email Already Exist....";
+        } else {
+            $query = "INSERT INTO `login` (`id`, `name`, `email`, `password`) VALUES ('1','$name','$email','$pwd')";
+            mysqli_query($con, $query);
+            echo "Created....";
+        }
+    }
+} else {
+    echo "Click on Agree";
+}
+include("../Ecommerce-website/index.html");
 ?>
