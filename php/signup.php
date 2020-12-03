e11f836a378670b12931f2bbe498f9525ce48cb9
Chand <meetchchand@gmail.com>
2020-12-03 18:24:39 +0500

    Updated Sign Up Form

--- php/signup.php
+++ php/signup.php
@@ -1,31 +1,32 @@
 <?php
-
-$chk = $_POST["check"];
-if ($chk == 'on') {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $pwd = $_POST["pwd"];
-
-
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "sablayja";
-
-    $con = mysqli_connect($servername, $username, $password, $database) or die("Not Connected");
-    if ($con->connect_error == false) {
-        $query = "SELECT * FROM 'login' WHERE 'email'='$email'";
-        $res = mysqli_query($con, $query);
-        if (mysqli_num_rows($res) > 0) {
-            echo "Email Already Exist....";
-        } else {
-            $query = "INSERT INTO `login` (`id`, `name`, `email`, `password`) VALUES ('1','$name','$email','$pwd')";
-            mysqli_query($con, $query);
-            echo "Created....";
+if(isset($_POST['submit'])){
+    if (isset($_POST["check"])) {
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $pwd = $_POST["pwd"];
+    
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "sablayja";
+    
+        $con = mysqli_connect($servername, $username, $password, $database) or die("Not Connected");
+        if ($con->connect_error == false) {
+            $query = "SELECT * FROM 'login' WHERE 'email'='$email'";
+            $res = mysqli_query($con, $query);
+            if (mysqli_num_rows($res) == 0) {
+                $query = "INSERT INTO `login` (`name`, `email`, `password`) VALUES ('$name','$email','$pwd')";
+                mysqli_query($con, $query);
+                echo "Created....";
+            } else {
+                echo "Email Already Exist....";
+            }
         }
+    } else {
+        echo "Click on Agree";
     }
-} else {
-    echo "Click on Agree";
 }
-include("../Ecommerce-website/index.html");
+else{
+    echo "Re_Inter Data....";
+}
 ?>
