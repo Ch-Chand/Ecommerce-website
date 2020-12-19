bfa2515d12c50dfab4b049262a52daca15180298
Chand <meetchchand@gmail.com>
2020-12-19 16:40:21 +0500

    Worked on Login using AJAX

--- js/main.js
+++ js/main.js
@@ -31,3 +31,50 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+/* Sign In, Up Forms */
+
+function Creation() {
+    val1 = document.getElementById("name2").value;
+    val2 = document.getElementById("email2").value;
+    val3 = document.getElementById("pwd2").value;
+    val4 = document.getElementById("check2").value;
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200)
+            res = this.responseText;
+    };
+    xmlhttp.open("GET", "php/signup.php?v1=" + val1 + "&v2=" + val2 + "&v3=" + val3 + "&v4=" + val4, true);
+    xmlhttp.send();
+
+    alert(res);
+
+    if (res == "Yes") {
+        document.getElementById("sign_up_button").style.display = "none";
+        document.getElementById("sign_in_button").style.display = "none";
+        document.getElementById("account").innerHTML = "Hi! User";
+    }
+    if(res=="No"){
+        alert("Try with another Email");
+    }
+}
+
+
+
+function Validation() {
+    va1 = document.getElementById("email1").value;
+    va2 = document.getElementById("pwd1")
+
+    alert("va1");
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200)
+            document.getElementById("myspan1").innerHTML = this.responseText;
+    };
+
+    xmlhttp.open("GET", "ajaxdbinsert.php?v1=" + rr + "&v2=" + nn + "&v3=" + mm, true);
+    xmlhttp.send();
+
+}
--- php/signup.php
+++ php/signup.php
@@ -1,32 +1,23 @@
 <?php
-if(isset($_POST['submit'])){
-    if (isset($_POST["check"])) {
-        $name = $_POST["name"];
-        $email = $_POST["email"];
-        $pwd = $_POST["pwd"];
-    
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $database = "sablayja";
-    
-        $con = mysqli_connect($servername, $username, $password, $database) or die("Not Connected");
-        if ($con->connect_error == false) {
-            $query = "SELECT * FROM 'login' WHERE 'email'='$email'";
-            $res = mysqli_query($con, $query);
-            if (mysqli_num_rows($res) == 0) {
-                $query = "INSERT INTO `login` (`name`, `email`, `password`) VALUES ('$name','$email','$pwd')";
-                mysqli_query($con, $query);
-                echo "Created....";
-            } else {
-                echo "Email Already Exist....";
-            }
-        }
+$name = $_GET["v1"];
+$email = $_GET["v2"];
+$pwd = $_GET["v3"];
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "sablayja";
+
+$con = mysqli_connect($servername, $username, $password, $database) or die("Not Connected");
+if ($con->connect_error == false) {
+    $query = "SELECT * FROM 'login' WHERE 'email'='$email'";
+    $res = mysqli_query($con, $query);
+    if (mysqli_num_rows($res) == 0) {
+        $query = "INSERT INTO `login` (`name`, `email`, `password`) VALUES ('$name','$email','$pwd')";
+        mysqli_query($con, $query);
+        echo "Yes";
     } else {
-        echo "Click on Agree";
+        echo "No";
     }
-}
-else{
-    echo "Re_Inter Data....";
 }
 ?>
